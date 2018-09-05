b09a921b9a1e8ceff84d90dfcbe0f327d2c3c525
James W. Bradley <jwbradley@users.noreply.github.com>
2018-09-05 08:34:28 -0500

    Removed redundancies in the code
    Removed the methods that were not really needed to get the same results.

--- classes/Benchmark.php
+++ classes/Benchmark.php
@@ -2,29 +2,17 @@
 
 class Benchmark
 {
-    public $start;
-    public $end;
-
-    public function start()
-    {
-        $this->start = microtime(true);
+    __construct() {
+	$this->start = microtime(true);	    
     }
-
-    public function end()
-    {
-        $this->end = microtime(true);
-    }
+	
     public function elapsedTime()
     {
         return $form->format_period((microtime(true))  - $this->start)
     }
-    public function totalTime()
-    {
-        return $form->format_period($this->end - $this->start);
-    }
     
-	function format_period($seconds_input) {
-		$hours = (int) ($minutes = (int) ($seconds = (int) ($milliseconds = (int) ($seconds_input * 1000)) / 1000) / 60) / 60;
-		return  (int) $hours . ':' . ($minutes % 60) . ':' . ($seconds % 60) . (($milliseconds === 0) ? '' : '.' . rtrim($milliseconds % 1000, '0'));
-	}
+    function format_period($seconds_input) {
+	$hours = (int) ($minutes = (int) ($seconds = (int) ($milliseconds = (int) ($seconds_input * 1000)) / 1000) / 60) / 60;
+	return  (int) $hours . ':' . ($minutes % 60) . ':' . ($seconds % 60) . (($milliseconds === 0) ? '' : '.' . rtrim($milliseconds % 1000, '0'));
+    }
 }
